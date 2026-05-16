f24110b945d4529c30e96ec82bcc6959f4536ea5
Sahar Zahir <sazahir@ucsd.edu>
2026-05-15 20:50:15 -0700

    Update map.js

--- map.js
+++ map.js
@@ -5,7 +5,8 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
 // Set your Mapbox access token here
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2F6YWhpciIsImEiOiJjbXA3c3dvbDAwNWhjMnNwcnBvd2d0c3UxIn0.m-59WkpO8LxsjR36mf6Q9w';
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2F6YWhpciIsImEiOiJjbXA3c3dvbDAwNWhjMnNwcnBvd2d0c3UxIn0.m-59WkpO8LxsjR36mf6Q9w';
 
 // Initialize the map
 const map = new mapboxgl.Map({
@@ -15,4 +16,40 @@ const map = new mapboxgl.Map({
   zoom: 12,
   minZoom: 5,
   maxZoom: 18,
+});
+
+map.on('load', async () => {
+  // Boston bike lanes
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+
+  // Cambridge bike lanes
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/vis-society/lab7/main/data/cambridge-bike-lanes.geojson',
+  });
+
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
 });
