aa3fe663abf5170c76ca3ed6e3b92c54304da4ea
Sahar Zahir <sazahir@ucsd.edu>
2026-05-15 20:40:56 -0700

    lab 7

---
+++ map.js
@@ -0,0 +1,18 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'PASTE_YOUR_MAPBOX_ACCESS_TOKEN_HERE';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
